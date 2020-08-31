2ac9a94097c51a45df1f796beb79cc04e2bbf774
Joshua Lovelace <rakmial@gmail.com>
2020-08-31 14:05:58 -0700

    test 2 green

--- lib/CoinPurse.rb
+++ lib/CoinPurse.rb
@@ -2,9 +2,10 @@ require('rspec')
 require('pry')
 
 class CoinPurse
-  attr_reader(:change_in)
+  attr_reader(:change_in, :denominations)
 
   def initialize(change_in)
     @change_in = change_in
+    @denominations = [25, 10, 5, 1]
   end
 end
