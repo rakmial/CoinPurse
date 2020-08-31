a0b1e38d677d9372c6fec39b9b04193988a7725f
Joshua Lovelace <rakmial@gmail.com>
2020-08-31 14:37:01 -0700

    minimum test 3 green

--- lib/CoinPurse.rb
+++ lib/CoinPurse.rb
@@ -8,4 +8,20 @@ class CoinPurse
     @change_in = change_in
     @denominations = [25, 10, 5, 1]
   end
+
+  def change_out
+    remainder = @change_in
+    i = 0
+    change = [[0,' quarters'],
+              [0,' dimes'],
+              [0,' nickels'],
+              [0,' pennies']]
+    
+    @denominations.each do |denomination|
+      change[i][0] = remainder / denomination
+      remainder -= change[i][0] * denomination
+      i += 1
+    end
+    change.flat_map { |e| e + ["\n"]}.join
+  end
 end
