f72995c09ca0d18b96e325572bf2138cce2b5982
Joshua Lovelace <rakmial@gmail.com>
2020-08-31 14:12:02 -0700

    test 3 red

--- spec/CoinPurse_spec.rb
+++ spec/CoinPurse_spec.rb
@@ -8,4 +8,7 @@ describe(CoinPurse) do
   it('provides attr_reader(:denominations) for instance variable denominations = [25, 10, 5, 1]') do
     expect(CoinPurse.new(1).denominations).to(eq([25, 10, 5, 1]))
   end
+  it('provides method .change_out(change_in), which accurately returns minimum combination of coins for change_in') do
+    expect(CoinPurse.new(1).change_out).to(eq("0 quarters\n0 dimes\n0 nickels\n1 pennies\n"))
+  end
 end
