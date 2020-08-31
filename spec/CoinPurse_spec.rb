30d7ada19f87453a036bd77781311f214b4299ed
Joshua Lovelace <rakmial@gmail.com>
2020-08-31 14:05:18 -0700

    test 2 red

--- spec/CoinPurse_spec.rb
+++ spec/CoinPurse_spec.rb
@@ -5,4 +5,7 @@ describe(CoinPurse) do
   it('initializes with integer input change_in with attr_reader(:change_in)') do
     expect(CoinPurse.new(1).change_in).to(eq(1))
   end
+  it('provides attr_reader(:denominations) for instance variable denominations = [25, 10, 5, 1]') do
+    expect(CoinPurse.new(1).denominations).to(eq([25, 10, 5, 1]))
+  end
 end
