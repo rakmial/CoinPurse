321fce3270b3bb659397df6c4593ea7714220293
Joshua Lovelace <rakmial@gmail.com>
2020-08-31 14:02:20 -0700

    fixed test 1, test 1 green

--- lib/CoinPurse.rb
+++ lib/CoinPurse.rb
@@ -2,5 +2,9 @@ require('rspec')
 require('pry')
 
 class CoinPurse
+  attr_reader(:change_in)
 
+  def initialize(change_in)
+    @change_in = change_in
+  end
 end
--- spec/CoinPurse_spec.rb
+++ spec/CoinPurse_spec.rb
@@ -3,6 +3,6 @@ require('CoinPurse')
 
 describe(CoinPurse) do
   it('initializes with integer input change_in with attr_reader(:change_in)') do
-    expect(CoinPurse(1).change_in).to(eq(1))
+    expect(CoinPurse.new(1).change_in).to(eq(1))
   end
 end
